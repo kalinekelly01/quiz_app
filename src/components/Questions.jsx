704ba6fb89d96d2674cb0629c78ad8683323157f
Kaline Kelly <145710571+kalinekelly01@users.noreply.github.com>
2024-01-10 15:51:12 -0300

    Add files via upload

---
+++ src/components/Questions.jsx
@@ -0,0 +1,41 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+import Option from './Option';
+import "./Questions.css";
+
+const Questions = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+
+    const currentQuestions = quizState.questions[quizState.currentQuestions];
+
+    const onSelectOption = (option) => {
+      dispatch({
+        type: "CHECK_ANSWER",
+        payload: { answer: currentQuestions.answer, option },
+      });
+    };
+
+  return (
+  <div id='questions'>
+    <p>Pergunta {quizState.currentQuestions + 1} de {quizState.questions.length}</p>
+   <h2>{currentQuestions.question}</h2>
+    <div id="options-container">
+        {currentQuestions.options.map((option) => (
+          <Option option={option} key={option} answer={currentQuestions.answer} selectOption={() => onSelectOption(option)} /> 
+        ))}
+    </div>
+    {!quizState.answerSelected && !quizState.help && (
+      <>{currentQuestions.tip && (<button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
+       )}
+      </>
+    )}
+  {!quizState.answerSelected && quizState.help === "tip" && (<p>{currentQuestions.tip}</p>
+  )}
+   {quizState.answerSelected && (
+     <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>Continuar</button>
+   )}
+  </div>
+  );
+};
+
+export default Questions;
---
+++ src/context/quiz.jsx
@@ -0,0 +1,100 @@
+import { createContext, useReducer } from "react";
+import questions from "../data/questions_complete";
+
+const STAGES = ["Start","Category", "Playing", "End"];
+
+const inicialState = {
+    gameStage: STAGES[0],
+    questions,
+    currentQuestions: 0,
+    score: 0,
+    answerSelected: false,
+    help: false,
+};
+
+const quizReducer = (state, action) => {
+   
+    switch(action.type) {
+        case "CHANGE_STATE":
+          return {
+            ...state,
+            gameStage: STAGES[1],
+          };
+
+          case "START_GAME":
+          let quizQuestions = null
+
+          state.questions.forEach((question) => {
+            if(question.category === action.payload) {
+                quizQuestions = question.questions;
+            }
+          });
+
+         return {
+            ...state,
+            questions: quizQuestions,
+            gameStage: STAGES[2],
+         };
+
+          case "REORDER_QUESTIONS":
+            const reorderedQuestions = state.questions.sort(() => {
+                return Math.random() - 0.5;
+            });
+
+            return {
+                ...state,
+                questions: reorderedQuestions,
+            };
+
+            case "CHANGE_QUESTION":
+                const nexQuestions = state.currentQuestions + 1;
+                let endGame = false;
+
+                if (!state.questions[nexQuestions]) {
+                    endGame = true;
+                };
+
+                return {
+                    ...state,
+                    currentQuestions: nexQuestions,
+                    gameStage: endGame ? STAGES[3] : state.gameStage,
+                    answerSelected: false,
+                    help: false,
+                };
+
+                case "NEW_GAME":
+                    return inicialState;
+
+                case "CHECK_ANSWER":
+                    if(state.answerSelected) return state;
+
+                const answer = action.payload.answer;
+                const option = action.payload.option;
+                let correctAnswer = 0;
+
+                if (answer === option) correctAnswer = 1;
+
+                return {
+                    ...state,
+                    score: state.score + correctAnswer,
+                    answerSelected: option,
+                };
+
+                case "SHOW_TIP":
+                    return {
+                    ...state,
+                    help: "tip",
+                    };
+
+         default:
+         return state;
+    };
+}; 
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({children}) => {
+    const value = useReducer(quizReducer, inicialState);
+
+    return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+};
